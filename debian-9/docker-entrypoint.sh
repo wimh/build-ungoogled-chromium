fae3def0f0b19dc8aa1d92fdc15119a7913e1f09
wimh <wimh@users.noreply.github.com>
2017-01-27 23:34:07 +0100

    fix entrypoint, improve buildscript

--- debian-9/docker-entrypoint.sh
+++ debian-9/docker-entrypoint.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
+cd /
 git clone https://github.com/Eloston/ungoogled-chromium.git
-cd ungoogled-chromium
+cd /ungoogled-chromium
 
-if [ "$1" == "bash" ]; then
-  exec bash
-elif [ -n "$1" ]; then
-  git checkout $1
-fi
+for arg in "$@"
+do
+  case "$arg" in
+    bash)
+      # run bash
+      bash
+      ;;
+    static)
+      # 'hide' lsb_release command to avoid detection of distribution
+      # this will will cause the build to be run as 'other' linux
+      ln -s /bin/true /usr/local/sbin/lsb_release
+      ;;
+    dynamic)
+      # 'unhide' lsb_release, so build will be run as debian
+      rm -f /usr/local/sbin/lsb_release
+      ;;
+    *)
+      cd /ungoogled-chromium
 
-python3 ./build.py
+      # cleanup files from previous build, except downloads
+      git ls-files --others --exclude="build/download/*" | xargs --no-run-if-empty rm -f
+      find . -type d -empty -delete
+      git checkout -- .
 
-if [ -d /output ]; then
-  mv build/*.tar.xz /output
-fi
+      # checkout branch/tag/hash
+      git checkout "$arg"
+      if [ $? -ne 0 ]; then
+        echo "cannot checkout $arg"
+        exit 1
+      fi
 
-exec bash
+      # fix path to clang in some tags
+      grep -q clang_base_path resources/linux_static/gn_flags
+      if [ $? -ne 0 ]; then
+        echo 'clang_base_path="/usr"' >> resources/linux_static/gn_flags
+      fi
+
+      # now build
+      python3 ./build.py
+
+      # move output file to mounted host directory
+      if [ -d /output ]; then
+        mv build/*.tar.xz /output
+      fi
+      ;;
+  esac
+done
+
+exit 0
